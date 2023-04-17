70fd2b232ce701bcf5de15499f75bafdbb951fe7
Nikolai Tarasov <125746816+greasemonkey-nt@users.noreply.github.com>
2023-04-17 17:35:03 +0300

    Update test_HH.py

--- PythonProjects/Requests/Tests/test_HH.py
+++ PythonProjects/Requests/Tests/test_HH.py
@@ -13,7 +13,7 @@ def test_check_testing_1():
     params = {'text': 'Функциональное тестирование'})
     assert response.json()['items'] == [{'id': '3007', 'text': 'Функциональное тестирование'}]
 
-# Проверяем что в выдаче по квери 'Функциональное тестирование' есть 'items' с конкретным номером 'id' и значением 'text'
+# Проверяем что в выдаче по квери 'Регрессионное тестирование' есть 'items' с конкретным номером 'id' и значением 'text'
 def test_check_testing_2():
     response = requests.get('https://api.hh.ru/suggests/skill_set',
     params = {'text': 'Регрессионное тестирование'})
